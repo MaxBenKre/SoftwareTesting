8992c6edd27a4167ebe51181ea20e379f979674e
Maximilian Kreutner <maxi.kreutner@gmail.com>
2019-10-15 17:13:48 +0200

    Done

--- st-2019-junit5-intro-MaxBenKre-master/st-2019-junit5-intro-MaxBenKre-master/src/test/java/de/uni_passau/fim/se2/junit5_intro/LiftTest.java
+++ st-2019-junit5-intro-MaxBenKre-master/st-2019-junit5-intro-MaxBenKre-master/src/test/java/de/uni_passau/fim/se2/junit5_intro/LiftTest.java
@@ -1,5 +1,69 @@
 package de.uni_passau.fim.se2.junit5_intro;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
 class LiftTest {
+
+    private Lift lift;
+    private Lift biggerLift;
+
+    @BeforeEach
+    void init(){
+        lift = new Lift(5);
+        biggerLift = new Lift(10, 10);
+    }
+
+    @Test
+    void testGetFloor(){
+        assertEquals(5, lift.getTopFloor());
+    }
+
+    @Test
+    void testgetCurrentFloor(){
+        assertEquals(0, lift.getCurrentFloor());
+    }
+
+    @Test
+    void testGetCapacity(){
+        assertEquals(10, biggerLift.getCapacity());
+    }
+
+    @Test
+    void testGetRiders(){
+        assertEquals(0, lift.getNumRiders());
+    }
+
+    @Test
+    void testGoUpAndDown(){
+        lift.goUp();
+        lift.goUp();
+        lift.goDown();
+        assertEquals(1, lift.getCurrentFloor());
+    }
+
+    @Test
+    void testIsFull(){
+        assertFalse(lift.isFull());
+    }
+
+    @Test
+    void testAddRiders(){
+        assertAll("Riders",
+                () -> lift.addRiders(1),
+                () -> assertEquals(1, lift.getNumRiders()),
+                () -> lift.addRiders(20),
+                () -> assertEquals(10, lift.getNumRiders()));
+    }
+
+    @Test
+    void testCall(){
+        assertAll("Riders",
+                () -> lift.call(4),
+                () -> assertEquals(4, lift.getCurrentFloor()),
+                () -> lift.call(1),
+                () -> assertEquals(1, lift.getCurrentFloor()));
+    }
 
 }
--- st-2019-junit5-intro-MaxBenKre-master/st-2019-junit5-intro-MaxBenKre-master/src/test/java/de/uni_passau/fim/se2/junit5_intro/MiniMathTest.java
+++ st-2019-junit5-intro-MaxBenKre-master/st-2019-junit5-intro-MaxBenKre-master/src/test/java/de/uni_passau/fim/se2/junit5_intro/MiniMathTest.java
@@ -1,5 +1,49 @@
 package de.uni_passau.fim.se2.junit5_intro;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
 class MiniMathTest {
+
+    private MiniMath miniMath;
+
+    @BeforeEach
+    void init(){
+        miniMath = new MiniMath();
+    }
+
+    @Test
+    void testMin(){
+        assertAll("min",
+                () -> assertEquals(10, miniMath.min(10, 20)),
+                () -> assertEquals(10, miniMath.min(20, 10)));
+
+    }
+
+    @Test
+    void testMax(){
+        assertAll("max",
+                () -> assertEquals(20, miniMath.max(10, 20)),
+                () -> assertEquals(20, miniMath.max(20, 10)));
+
+    }
+
+    @Test
+    void testAbs(){
+        assertAll("abs",
+                () -> assertEquals(20, miniMath.abs(-20)),
+                () -> assertEquals(20, miniMath.abs(20)));
+
+    }
+
+    @Test
+    void testFloor(){
+        assertAll("floor",
+                () -> assertEquals(20, miniMath.floorDivision(200, 10)),
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> miniMath.floorDivision(200, 0)));
+
+    }
 
 }
